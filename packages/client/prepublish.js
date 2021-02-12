b8a1f3819dc41e3f1f27ad8368fbd2eba1604b8b
Forbes Lindesay <forbes@lindesay.co.uk>
2021-02-12 14:50:29 +0000

    ci: add assertions

--- packages/client/prepublish.js
+++ packages/client/prepublish.js
@@ -1,3 +1,8 @@
+const assert = require('assert');
+const fs = require('fs');
+
+const isVersion = /^\d+\.\d+\.\d+(\-canary\-\d+)?$/.test(env.NEW_VERSION);
+
 const env = {};
 for (const key of [
   'CANARY',
@@ -6,13 +11,24 @@ for (const key of [
   'GITHUB_REPOSITORY',
   'GITHUB_REPOSITORY_OWNER',
   'GITHUB_REPOSITORY_NAME',
-  'DEPENDENCY_ROLLINGVERSIONS_TEST_CUSTOM_SCRIPTS_NPM_GITHUB_ACTIONS_A',
-  'DEPENDENCY_ROLLINGVERSIONS_TEST_CUSTOM_SCRIPTS_NPM_GITHUB_ACTIONS_B',
   'DEPENDENCY_MY_CUSTOM_API',
+  'DEPENDENCY_MY_CUSTOM_CLIENT',
   'NEW_VERSION',
 ]) {
   env[key] = process.env[key];
 }
+
+assert(env.CANARY === undefined || /^\d+$/.test(env.CANARY));
+assert.strictEqual(
+  env.GITHUB_REPOSITORY,
+  'RollingVersions/test-custom-tag-format'
+);
+assert.strictEqual(env.GITHUB_REPOSITORY_OWNER, 'RollingVersions');
+assert.strictEqual(env.GITHUB_REPOSITORY_NAME, 'test-custom-tag-format');
+
+assert(isVersion(env.DEPENDENCY_MY_CUSTOM_API));
+assert(isVersion(env.DEPENDENCY_MY_CUSTOM_CLIENT));
+assert(isVersion(env.NEW_VERSION));
 
 const majorVersion = parseInt(process.env.NEW_VERSION.split('.')[0], 10);
 if (majorVersion % 2 === 0) {
@@ -21,3 +37,5 @@ if (majorVersion % 2 === 0) {
   );
   process.exit(1);
 }
+
+fs.writeFileSync(`${__dirname}/prepublish-env.json`, JSON.stringify(env));
--- packages/client/publish.js
+++ packages/client/publish.js
@@ -1,3 +1,8 @@
+const assert = require('assert');
+const fs = require('fs');
+
+const isVersion = /^\d+\.\d+\.\d+(\-canary\-\d+)?$/.test(env.NEW_VERSION);
+
 const env = {};
 for (const key of [
   'CANARY',
@@ -6,13 +11,30 @@ for (const key of [
   'GITHUB_REPOSITORY',
   'GITHUB_REPOSITORY_OWNER',
   'GITHUB_REPOSITORY_NAME',
-  'DEPENDENCY_ROLLINGVERSIONS_TEST_CUSTOM_SCRIPTS_NPM_GITHUB_ACTIONS_A',
-  'DEPENDENCY_ROLLINGVERSIONS_TEST_CUSTOM_SCRIPTS_NPM_GITHUB_ACTIONS_B',
   'DEPENDENCY_MY_CUSTOM_API',
+  'DEPENDENCY_MY_CUSTOM_CLIENT',
   'NEW_VERSION',
 ]) {
   env[key] = process.env[key];
 }
+
+assert(env.CANARY === undefined || /^\d+$/.test(env.CANARY));
+assert.strictEqual(
+  env.GITHUB_REPOSITORY,
+  'RollingVersions/test-custom-tag-format'
+);
+assert.strictEqual(env.GITHUB_REPOSITORY_OWNER, 'RollingVersions');
+assert.strictEqual(env.GITHUB_REPOSITORY_NAME, 'test-custom-tag-format');
+
+assert(isVersion(env.DEPENDENCY_MY_CUSTOM_API));
+assert(isVersion(env.DEPENDENCY_MY_CUSTOM_CLIENT));
+assert(isVersion(env.NEW_VERSION));
+
+assert.deepStrictEqual(
+  JSON.parse(JSON.stringify(env)),
+  JSON.parse(fs.readFileSync(`${__dirname}/prepublish-env.json`, `utf8`))
+);
+
 if (process.argv.includes('--dry-run')) {
   console.log('DEPLOYING CLIENT (dry run)', env);
 } else {
